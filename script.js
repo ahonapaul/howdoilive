0bca19993936e40bad55b61314f7cf413a7d1dfd
Ahona Paul <paulahona03@gmail.com>
2023-10-18 14:23:49 -0400

    new js code

--- script.js
+++ script.js
@@ -7,24 +7,58 @@ const day1 = {
             let stomachcap = [0, 1];
             notfood = Math.floor(Math.random(stomachcap) * 2);
             if(notfood == 0){
-                console.log("feeling good");
+                console.log("full");
                 return;
             }
             if(notfood == 1){
-                console.log("feeling weird");
+                console.log("hungry");
                 return;
             }
         },
     },
     
-    sleep: "I never get sleep",
+    sleep: 
+    {
+        time: 0.0,
+        disturbances: 2,
+        feeling: function(sleeptime){
+            let sleephours = [3, 4, 5, 6, 7];
+            sleeptime = Math.floor(Math.random(sleephours) * sleephours.length);
 
-    social: {
+            if(sleeptime == 0){
+                console.log("feeling dead");
+                return;
+            }
+
+            if(sleeptime == 1){
+                console.log("feeling woozy");
+                return;
+            }
+
+            if(sleeptime == 2){
+                console.log("feeling okay");
+                return;
+            }
+
+            if(sleeptime == 3){
+                console.log("feeling good");
+                return;
+            }
+
+            if(sleeptime == 4){
+                console.log("feeling refreshed");
+                return;
+            }
+        }
+    },
+
+    social: 
+    {
         time: 2.75,
         people: [name1, name2, name3],
         feeling: function(talktime){
             let talkhours = [0, 1, 2, 3, 4];
-            talktime = Math.floor(Math.random(hours) * 5);
+            talktime = Math.floor(Math.random(talkhours) * 5);
 
             if(talktime == 0){
                 console.log("battery 100%");
@@ -36,9 +70,21 @@ const day1 = {
                 return;
             };
 
-            //and so on
+            if(talktime == 2){
+                console.log("battery 60%");
+                return;
+            }
+
+            if(talktime == 3){
+                console.log("battery 45%");
+                return;
+            }
+
+            if(talktime == 4){
+                console.log("battery 20%");
+                return;
+            }
         }
     },
-    //and more if i can think of some more factors
 };
 // console.log() can be changed with other things that can execute for final version
